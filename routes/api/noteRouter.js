08bbc04b29dae1064ce66038bf77f401baa2cfbc
Dylan Storm Johnson <dylanstormjohnson@comcast.net>
2023-05-08 17:51:10 -0500

    Added getNotes functionality and addNotes functionality to the api noteRouter

--- routes/api/noteRouter.js
+++ routes/api/noteRouter.js
@@ -1,26 +1,52 @@
 const router = require("express").Router()
 const path = require("path")
+const fs = require("fs/promises")
+const { v4: uuidv4 } = require('uuid');
+const readNotes = async () => {
+    var originalFile = await fs.readFile(path.join(__dirname, "../../db/db.json"));
+    originalFile = JSON.parse(originalFile)
+    return originalFile
+}
+
 router.get('/', async (req, res) => {
     // console.log('this is the /api/notes route');
     try {
-        const originalFile = await fs.readFile('./db/db.json');
-
-        const data = JSON.parse(originalFile);
-
-        console.log(data);
+        res.json(await readNotes())
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
     }
-    res.sendFile(path.join(__dirname, './db/db.json'))
 });
 
-router.post('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './db/db.json'))
+router.post('/', async (req, res) => {
+    try {
+        console.log("Posting to my notes")
+        console.log(req.body)
+        var newNote = {...req.body,
+        id: uuidv4()}
+        var notes = await readNotes()
+        notes.push(newNote)
+        await fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(notes));
+        res.json({message: "You're doing great!"})
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+
 });
 
-router.delete('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './db/db.json'))
+router.delete('/:id', async (req, res) => {
+    try {
+        var notes = await readNotes()
+        //find the particular note with the id, and take it out
+        
+
+        await fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(notes));
+        res.json({message: "You're doing great!"})
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router
